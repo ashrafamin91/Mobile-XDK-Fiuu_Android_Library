42f01a178cf31c581ff24d12ad4a845f44670b63
Ashraf Amin <ashraf.amin@razer.com>
2025-04-29 18:39:37 +0800

    Fixed version

--- paymentxdk/src/main/java/com/molpay/molpayxdk/MOLPayActivity.java
+++ paymentxdk/src/main/java/com/molpay/molpayxdk/MOLPayActivity.java
@@ -128,7 +128,7 @@ public class MOLPayActivity extends AppCompatActivity {
     private final static String mpclickgpbutton = "mpclickgpbutton://";
     private final static String module_id = "module_id";
     private final static String wrapper_version = "wrapper_version";
-    private final static String wrapperVersion = "15a-Beta";
+    private final static String wrapperVersion = "15a-beta";
 
     private String filename;
     private Bitmap imgBitmap;
